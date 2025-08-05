1ff086351297c8ceb3ccc09b5d00c3881298abe0
hdongqt <hdongqt@gmail.com>
2025-08-05 20:12:22 +0700

    feat(recipe-view): "refactor mvc view"

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,41 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+
 import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second !`));
-    }, s * 1000);
-  });
+const showRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    await model.loadRecipe(id);
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+    recipeView.renderError();
+  }
 };
+
+const controlSearchResults = async function () {
+  try {
+    const query = document.querySelector('.search__field').value;
+    if (!query) return;
+    await model.loadSearchResult('pizza');
+    searchView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(showRecipe);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+
+init();
